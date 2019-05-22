6a03e1f9d34f655877ac52a0cec1979068ba42e5
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-05-22 23:26:47 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,16 @@
 class Dog
+  attr_accessor :name, :breed, :age
+  @@all = []
 
-de
+  def initialization(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
 
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
